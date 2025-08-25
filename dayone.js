adc404d2fa6592b2189e232fc704919c14772ab3
Yashvishwakarma2 <yyaasshh913@gmail.com>
2025-08-25 21:29:52 +0530

    Add files via upload

---
+++ dayone.js
@@ -0,0 +1,16 @@
+// Create a student object
+const student = {
+  name: "Yash Vishwakarma",
+  age: 20,
+  course: "B.Tech CSE",
+  rollNo: "12345",
+  college: "SISTec, Bhopal"
+};
+
+// Print student details
+console.log("Student Details:");
+console.log("Name: " + student.name);
+console.log("Age: " + student.age);
+console.log("Course: " + student.course);
+console.log("Roll No: " + student.rollNo);
+console.log("College: " + student.college);
